3530f68ef3c3f3794c0ad79884fc8e4e9991451d
Costinnn <crauciuc.costin@yahoo.com>
2023-01-20 20:13:42 +0200

    s2 responsive design

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
       {authReady && (
         <>
           <SectionOne />
-           {/* <SectionTwo /> */}
+           <SectionTwo />
           <SectionThree />
           <SectionFour />
         </>
--- src/Sections/SectionThree.jsx
+++ src/Sections/SectionThree.jsx
@@ -4,22 +4,43 @@ import ProjectDisplayTemplate from "../components/s3components/ProjectDisplayTem
 
 import PROJECTS_DATA from "../assets/s3/data/PROJECTS_DATA";
 
+import { useState } from "react";
+
+import upArrow from "../assets/s3/up-arrow.png";
+import downArrow from "../assets/s3/down-arrow.png";
+
 const SectionThree = () => {
+  const [count, setCount] = useState(3);
+
+  const viewMore = () => {
+    if (count === 3) {
+      setCount(8);
+    } else {
+      setCount(3);
+    }
+  };
+
   return (
     <div className="container s3" id="projects">
-      {PROJECTS_DATA.map((item) => {
-        return (
-          <ProjectDisplayTemplate
-            key={item.title1}
-            github={item.github}
-            link={item.link}
-            title1={item.title1}
-            img={item.img}
-            title2={item.title2}
-            text={item.text}
-          />
-        );
+      {PROJECTS_DATA.map((item, index) => {
+        while (index < count) {
+          return (
+            <ProjectDisplayTemplate
+              key={item.title1}
+              github={item.github}
+              link={item.link}
+              title1={item.title1}
+              img={item.img}
+              title2={item.title2}
+              text={item.text}
+            />
+          );
+        }
       })}
+      <div className="expand" onClick={viewMore}>
+        <img src={count === 3 ? downArrow : upArrow} alt="icon" />
+        <p>{count === 3 ? "more projects" : "less projects"}</p>
+      </div>
     </div>
   );
 };
